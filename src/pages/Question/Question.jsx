9d0149721ad0129294f6ed5a10db5f87d71b161a
Олексій Демидов <agdemidof@gmail.com>
2024-06-18 21:24:41 +0300

    Update Question.jsx

--- src/pages/Question/Question.jsx
+++ src/pages/Question/Question.jsx
@@ -49,7 +49,7 @@ function Question() {
     <div className="accordion-wrapper">
       <Accordion defaultActiveKey="none">
         <ContextAwareToggle eventKey="0">
-          <span>Як оформити замовлення??</span>
+          <span>Як оформити замовлення?</span>
         </ContextAwareToggle>
         <Accordion.Collapse eventKey="0">
           <Card.Body>
