dd6701f0d3664069692022b7cbef522f0403d5fd
Олексій Демидов <agdemidof@gmail.com>
2024-06-20 21:07:14 +0300

    fix

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,14 +1,18 @@
-// Feedback.jsx
-
 import React, { useEffect, useState } from 'react';
 import axios from '../../axios';
 import config from '../../config';
 import { useForm } from 'react-hook-form';
-import styles from './Feedback.module.scss'; // імпорт CSS-модуля
+import Slider from 'react-slick';
+import Modal from 'react-modal'; // імпорт компоненту модального вікна
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styles from './Feedback.module.scss';
 
 export const Feedback = () => {
   const { register, handleSubmit, reset } = useForm();
   const [feedbacks, setFeedbacks] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false); // стан для відкриття/закриття модального вікна
+  const [selectedImage, setSelectedImage] = useState(''); // для зберігання URL обраного зображення
   const userEmail = localStorage.getItem('adminEmail') || '';
 
   useEffect(() => {
@@ -29,7 +33,6 @@ export const Feedback = () => {
     formData.append('image', data.image[0]);
 
     try {
-      // Завантаження зображення на сервер
       const uploadResponse = await axios.post(
         `${config.baseURL}/upload`,
         formData,
@@ -42,7 +45,6 @@ export const Feedback = () => {
 
       const imageUrl = uploadResponse.data.url;
 
-      // Збереження URL зображення в базі даних
       const feedbackResponse = await axios.post(`${config.baseURL}/feedback`, {
         imageUrl,
       });
@@ -57,17 +59,8 @@ export const Feedback = () => {
 
   const handleDelete = async id => {
     try {
-      // Отримуємо URL зображення, щоб потім видалити його з сервера
-      const feedbackToDelete = feedbacks.find(feedback => feedback._id === id);
-      const imageUrlToDelete = feedbackToDelete.imageUrl;
-  
-      // Видаляємо відгук із сервера
       await axios.delete(`${config.baseURL}/feedback/${id}`);
-  
-      // Видаляємо зображення з сервера
-      await axios.delete(imageUrlToDelete);
-  
-      // Оновлюємо стан, видаляючи відгук із масиву feedbacks
+
       setFeedbacks(prevFeedbacks =>
         prevFeedbacks.filter(feedback => feedback._id !== id)
       );
@@ -76,24 +69,53 @@ export const Feedback = () => {
     }
   };
 
+  const openModal = imageUrl => {
+    setSelectedImage(imageUrl);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage('');
+    setModalIsOpen(false);
+  };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className={styles.feedbackContainer}>
       {userEmail === 'ivan@gmail.com' && (
-        <form
-          className={styles.formContainer}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <input
-            className={styles.fileInput}
-            type="file"
-            {...register('image')}
-          />
+        <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
+          <input className={styles.fileInput} type="file" {...register('image')} />
           <button className={styles.submitButton} type="submit">
             Завантажити зображення
           </button>
         </form>
       )}
-      <div className={styles.feedbackList}>
+      <Slider {...settings} className={styles.feedbackList}>
         {feedbacks.map(feedback => (
           <div key={feedback._id} className={styles.feedbackItem}>
             <img
@@ -101,6 +123,7 @@ export const Feedback = () => {
               src={`${config.baseURL}${feedback.imageUrl}`}
               alt="Feedback"
               className={styles.image}
+              onClick={() => openModal(`${config.baseURL}${feedback.imageUrl}`)}
             />
             {userEmail === 'ivan@gmail.com' && (
               <button
@@ -112,7 +135,20 @@ export const Feedback = () => {
             )}
           </div>
         ))}
-      </div>
+      </Slider>
+      {/* Модальне вікно */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Зображення"
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+        <button className={styles.closeButton} onClick={closeModal}>
+          &times;
+        </button>
+        <img crossOrigin="anonymous" src={selectedImage} alt="Modal" className={styles.modalImage} />
+      </Modal>
     </div>
   );
 };
