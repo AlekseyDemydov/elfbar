8a31b768be050f85689a938a45a8f22166c0e5b5
Олексій Демидов <agdemidof@gmail.com>
2024-06-29 15:16:49 +0300

    fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
     // baseURL: 'http://localhost:4444'
-    baseURL:process.env.REACT_APP_API_URL,
+    baseURL: 'https://elfbarr-a06f11b2b7c2.herokuapp.com'
+    // baseURL:process.env.REACT_APP_API_URL,
   };
   
   export default config;
