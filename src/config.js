275c02c0e8c585a5b5fb9ae61b31c89730eeeba1
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-07-17 11:09:36 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // baseURL: 'http://localhost:4444'
-    baseURL: 'https://elfbarr-1a9cad5b300f.herokuapp.com'
+    baseURL: 'https://elfbarr-64fed43ca1d5.herokuapp.com'
     // baseURL:process.env.REACT_APP_API_URL,
   };
   
