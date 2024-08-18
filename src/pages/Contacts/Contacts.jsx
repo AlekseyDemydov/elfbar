bb4472786288fcd18dc4f1d2388275dd0de53ac6
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-08-18 14:01:41 +0300

    fix

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -4,13 +4,26 @@ export const Contacts = () => {
   const handleClick = () => {
     window.open('https://t.me/anatolyi_st', '_blank');
   };
-
+  const handleClickGrupp = () => {
+    window.open('https://t.me/soaring78', '_blank');
+  };
+  const handleClickVBAdmin = () => {
+    window.open('viber://add?number=380994363382', '_blank');
+  };
   return (
     <>
       <div className={s.contactsBox}>
         <h1>Контакти</h1>
         <button className={s.btnContacts} onClick={handleClick}>
           <span>Зв'язатися з менеджером в Telegram</span>
+        </button>
+        <br />
+        <button onClick={handleClickVBAdmin} className={s.btnContactsVb}>
+        <span>Зв'язатися з менеджером в Viber</span>
+        </button>
+        <br />
+        <button className={s.btnContacts} onClick={handleClickGrupp}>
+          <span>Підписатися на Telegram</span>
         </button>
       </div>
     </>
